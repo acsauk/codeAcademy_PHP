8534a4840e02f612183b4396cdf3fdf6e9b530b7
acsauk <acsauk@gmail.com>
2016-10-03 20:51:42 +0100

    creates single page form using validations functions

--- forms/form_with_validation.php
+++ forms/form_with_validation.php
@@ -1,16 +1,40 @@
 <?php
   require_once("../building_webpages/included_functions.php");
+  require_once("validations_functions.php");
+
+  $errors = array();
+  $message = "";
 
   if (isset($_POST['submit'])) {
     // form was submitted
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    $username = trim($_POST["username"]);
+    $password = trim($_POST["password"]);
 
-    if ($username == "alex" && $password == "password123") {
-      // logged in successfully
-      redirect_to("../basic.html");
-    } else {
-      $message = "There were some errors.";
+    // Validations
+    $fields_required = array("username", "password");
+    foreach($fields_required as $field) {
+      $value = trim($_POST[$field]);
+      if (!validates_presence($value)) {
+        $errors[$field] = ucfirst($field) . " cannot be blank";
+      }
+    }
+
+    $fields_with_max_lengths = array("username" => 30, "password" => 8);
+    foreach($fields_with_max_lengths as $field => $max) {
+      $value = trim($_POST[$field]);
+      if (!validates_max_length($value, $max)) {
+        $errors[$field] = ucfirst($field) . " is too long";
+      }
+    }
+
+    if (empty($errors)) {
+      // Log in section of code
+      if ($username == "alex" && $password == "password123") {
+        // logged in successfully
+        redirect_to("../basic.html");
+      } else {
+        $message = "Username/Password do not match";
+      }
     }
   } else {
     $username = "";
@@ -26,11 +50,10 @@
   </head>
   <body>
 
-    <?php
-      echo $message;
-    ?>
+    <?php echo $message; ?>
+    <?php echo form_errors($errors); ?>
 
-    <form action="form_single.php" method="post">
+    <form action="form_with_validation.php" method="post">
       Username: <input type="text" name="username" value=<?php echo htmlspecialchars($username); ?> /><br />
       Password: <input type="password" name="password" value="" /><br />
       <br />
