54f3289d9a17ffcba6bb4efc6ebfba683e1c7e14
acsauk <acsauk@gmail.com>
2016-10-06 16:35:11 +0100

    adds project structure, drafts admin.php and stylesheet

---
+++ test_corp/public/admin.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Test Corp</title>
+    <link href="stylesheets/public.css" media="all"
+     rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="header">
+      <h1>Test Corp</h1>
+    </div>
+    <div id="main">
+      <div id="navigation">
+        &nbsp;
+      </div>
+      <div id="page">
+        <h2>Admin Menu</h2>
+        <p>Welcome to the admin area.</p>
+        <ul>
+          <li>
+            <a href="manage_content.php">Manage Website Content</a>
+          </li>
+          <li>
+            <a href="manage_admins.php">Manage Admin Users</a>
+          </li>
+          <li>
+            <a href="logout.php">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div id="footer">
+      Copyright 20xx, Test Corp
+    </div>
+
+  </body>
+</html>
